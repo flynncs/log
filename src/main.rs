ca6099eb0864c071acfbdc1c4e2ceea96ad4ef66
Flynn <flynn.teh@gmail.com>
2026-01-29 21:38:34 +1300

    feat: some form of error handling

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ async fn health() -> &'static str {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
     let app = Router::new()
@@ -36,5 +36,7 @@ async fn main() {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
